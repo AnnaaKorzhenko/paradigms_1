8d574114f296da617147d8037dddc7b9af3e04eb
Anna Korzhenko <akorzhenko@kse.org.ua>
2024-05-23 00:06:22 +0300

    created pseudocode structure

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 
+// memory_allocate(){}
+
+// memory_deallocate(){}
+
+// get_input(){}
+
 int main() {
-    std::cout << "Helloooo, World!" << std::endl;
+    // get user input
+
+    // do this:
+                // if command == 1 -> append_text(get user input)
+                // elif command == 2 -> start_new_line()
+                // elif command == 3 -> saving(get user input, file for saving)
+                // elif command == 4 -> loading(get user input, file for loading)
+                // elif command == 5 -> print_text()
+                // elif command == 6 -> insert_at_index(get user input, 2 integers for line and index; get user input, text to be inserted)
+                // elif command == 7 -> search(egt user input, text to be searched)
+                // elif command == 8 -> clear
+    // while command != quit
     return 0;
 }
